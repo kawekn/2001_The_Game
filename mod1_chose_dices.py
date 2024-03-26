2ba1f05c8f51a18ef7d78ada9293d6228d383b7a
norbert <nkawecki.tech@outlook.com>
2024-03-26 22:10:17 +0100

    Allow user chose dices - working

--- mod1_chose_dices.py
+++ mod1_chose_dices.py
@@ -18,16 +18,20 @@ def valid_dices(chosen_dice):
             return chosen_dice[1:]
 
 
-def chose_dice(dices_amount):
+def chose_dices(dices_amount, player=0):
     dices = []
     for dice_nr in range(1, dices_amount + 1):
-        while True:
-            try:
-                choice = input(f"Chose dice nr {dice_nr}: ")
-                dices.append(int(valid_dices(choice)))
-                break
-            except TypeError:
-                print("Wrong input")
+        if player == 0:
+            choice = random.choice(POSSIBLE_DICES)
+            dices.append(int(valid_dices(choice)))
+        else:
+            while True:
+                try:
+                    choice = input(f"Chose dice nr {dice_nr}: ")
+                    dices.append(int(valid_dices(choice)))
+                    break
+                except TypeError:
+                    print("Wrong input")
     return dices
 
 
@@ -35,9 +39,39 @@ def roll_dice(sides):
     return random.randint(1, sides)
 
 
-def game(dices_amount):
-    for sides in chose_dice(dices_amount):
-        (roll_dice(sides))
+def roll_dices(dices_amount, player, result=0,):
+    for sides in chose_dices(dices_amount, player):
+        result += (roll_dice(sides))
+    return result
 
 
+def score_after_1st_round(score, dices_amount, player):
+    if roll_dices(dices_amount, player, score) == 7:
+        score //= 7
+    elif roll_dices(dices_amount, player, score) == 11:
+        score *= 11
+    else:
+        score += roll_dices(dices_amount, player,score)
+    return score
 
+
+def rules_implementation(player_score=0, pc_score=0):
+    while True:
+        print(f"""
+        Player score: {player_score}
+            PC score: {pc_score}
+        """)
+        if player_score == 0 and pc_score == 0:
+            player_score = roll_dices(2, 1)
+            pc_score = roll_dices(2, 0)
+        elif player_score <= 2001 and pc_score <= 2001:
+            player_score = roll_dices(2, 1, player_score)
+            pc_score = roll_dices(2, 0,pc_score)
+        else:
+            if player_score >= 2001:
+                return f"The winner is player! Score: {player_score}"
+            else:
+                return f"The winner is PC! Score: {pc_score}"
+
+
+print(rules_implementation())
